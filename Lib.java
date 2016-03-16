3bd05c544fc5f90eff08b30cc2db0d6d6a364773
veronica <veronica.vigaray@gmail.com>
2016-03-17 00:12:49 +0100

    editado

--- Lib.java
+++ Lib.java
@@ -7,6 +7,7 @@ public class Lib {
 
     public void metodoAlumno1(){
         System.out.println("metodo 1"); //Muestra por pantalla
+	System.out.println("modificado por alumno1"); // alumno1
     }
 
     public void metodoAlumno2(){
@@ -14,6 +15,10 @@ public class Lib {
     }
 
     public void metodoComunitario(){
-        
+        // Comentario incluido para pruebas
+        System.out.println("Aqui escribimos todos");
+        // modificaciones alumno1
+        System.out.println("Alumno 1 inserta esta sentencia");
+
     }
 }
